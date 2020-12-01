dee4f07fca5d5582c40487f692183c05c7dc7c0f
Marshall Polaris <marshall@pol.rs>
2020-11-30 21:13:27 -0800

    Day 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# advent-2020
+Advent of Code 2020 solutions.
---
+++ day1/src/main.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::error::Error;
+use std::collections::HashSet;
+use std::io::{BufRead, BufReader};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = File::open("input")?;
+    let reader = BufReader::new(input);
+    let mut nums = HashSet::<i32>::new();
+    for line in reader.lines() {
+        nums.insert(line?.parse()?);
+    }
+
+    let total = 2020;
+    for n0 in &nums {
+        for n1 in &nums {
+            let candidate = total - n0 - n1;
+            if nums.contains(&candidate) {
+                println!("Result is: {}", n0 * n1 * candidate);
+                return Ok(());
+            }
+
+        }
+    }
+    Ok(())
+}
