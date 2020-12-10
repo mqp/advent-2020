0394a8c61ff4d2b81c8dc9160ebb0bf0671710e5
Marshall Polaris <marshall@pol.rs>
2020-12-10 14:31:38 -0800

    Clean up day 10

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -2,26 +2,26 @@ use std::error::Error;
 use std::io::{self, Read};
 use std::collections::HashMap;
 
-fn count_arrangements(counts_from: &mut HashMap<usize, usize>, adapters: &[usize], curr_idx: usize) -> usize {
-    match counts_from.get(&curr_idx) {
+fn count_chains(cache: &mut HashMap<usize, usize>, adapters: &[usize], i: usize) -> usize {
+    match cache.get(&i) {
         Some(&ct) => ct,
         None => {
-            let curr_adapter = adapters[curr_idx];
+            let voltage = adapters[i];
             let mut total = 0;
-            if curr_idx == adapters.len() - 1 {
+            if i == adapters.len() - 1 {
                 total += 1;
             }
-            if curr_idx + 1 < adapters.len() && adapters[curr_idx + 1] <= curr_adapter + 3 {
-                total += count_arrangements(counts_from, adapters, curr_idx + 1);
+            if i + 1 < adapters.len() && adapters[i + 1] <= voltage + 3 {
+                total += count_chains(cache, adapters, i + 1);
             }
-            if curr_idx + 2 < adapters.len() && adapters[curr_idx + 2] <= curr_adapter + 3 {
-                total += count_arrangements(counts_from, adapters, curr_idx + 2);
+            if i + 2 < adapters.len() && adapters[i + 2] <= voltage + 3 {
+                total += count_chains(cache, adapters, i + 2);
             }
-            if curr_idx + 3 < adapters.len() && adapters[curr_idx + 3] <= curr_adapter + 3 {
-                total += count_arrangements(counts_from, adapters, curr_idx + 3);
+            if i + 3 < adapters.len() && adapters[i + 3] <= voltage + 3 {
+                total += count_chains(cache, adapters, i + 3);
             }
-            counts_from.insert(curr_idx, total);
-            return total;
+            cache.insert(i, total);
+            total
         }
     }
 }
@@ -29,19 +29,12 @@ fn count_arrangements(counts_from: &mut HashMap<usize, usize>, adapters: &[usize
 fn main() -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    let mut adapter_voltages = Vec::new();
+    let mut adapters = vec!(0);
     for line in input.trim().split("\n") {
-        adapter_voltages.push(line.parse::<usize>()?);
+        adapters.push(line.parse()?);
     }
-    adapter_voltages.sort();
+    adapters.sort();
     let mut cache = HashMap::new();
-    let mut arrangements = count_arrangements(&mut cache, &adapter_voltages, 0);
-    if adapter_voltages[1] <= 3 {
-        arrangements += count_arrangements(&mut cache, &adapter_voltages, 1);
-    }
-    if adapter_voltages[2] <= 3 {
-        arrangements += count_arrangements(&mut cache, &adapter_voltages, 2);
-    }
-    println!("Result: {}", arrangements);
+    println!("Result: {}", count_chains(&mut cache, &adapters, 0));
     Ok(())
 }
